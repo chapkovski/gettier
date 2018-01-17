5e943f89f7d1832defc793aeec9b357141acb783
chapkovski <chapkovski@gmail.com>
2018-01-17 14:42:59 -0500

    importing chat routing instead of overriding

--- my_simple_survey/otree_extensions/routing.py
+++ my_simple_survey/otree_extensions/routing.py
@@ -1,9 +1,9 @@
 from channels.routing import route, route_class
 from my_simple_survey.consumers import ChatWatcher
-print('aaaa',ChatWatcher.url_pattern)
+from otree.chat.otree_extensions.routing import channel_routing as chat_routing
 # NOTE: otree_extensions is part of
 # otree-core's private API, which may change at any time.
-channel_routing = [
+channel_routing = chat_routing + [
     route_class(ChatWatcher, path=ChatWatcher.url_pattern),
 
 ]
